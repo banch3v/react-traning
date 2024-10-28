372ab69cd13781de3f6cc8ac0f4b40e7e42e6a40
banch3v <ivelin.banchev@gmail.com>
2024-10-28 11:25:30 +0200

    Complete players component

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editablePlayerName = isEditing ? (
+    <input
+      type="text"
+      value={playerName}
+      onChange={(event) => setPlayerName(event.target.value)}
+      required
+    />
+  ) : (
+    <span className="player-name">{playerName}</span>
+  );
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={() => setIsEditing((editing) => !editing)}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
